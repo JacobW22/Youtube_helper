509389b523d9db99554479c586b950ae3892b3ad
Jacob <79912360+JacobW22@users.noreply.github.com>
2023-08-08 20:16:27 +0200

    Dockerized app

--- app/ythelperapp/celery.py
+++ app/ythelperapp/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "youtube_helper.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.base")
 
 app = Celery("youtube_helper")
 
