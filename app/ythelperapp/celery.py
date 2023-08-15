41f77455092ebbe2211c9ccda85439029f9cc6c4
Jacob <79912360+JacobW22@users.noreply.github.com>
2023-08-15 20:22:24 +0200

    Saving avatars to bucket gallery

--- app/ythelperapp/celery.py
+++ app/ythelperapp/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.prod")
 
 app = Celery("youtube_helper")
 
