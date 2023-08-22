d388bcf9643097cbaac35c0d1e07fbbb4a221382
Jacob <79912360+JacobW22@users.noreply.github.com>
2023-08-22 20:24:20 +0200

    Changed how user storage work

--- app/settings/base.py
+++ app/settings/base.py
@@ -104,7 +104,7 @@ REST_FRAMEWORK = {
     ),
 
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permission.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticated'
     ),
 
     'PAGE_SIZE': 50
