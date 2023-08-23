43b4e44e022d0eea081fcbd5da84dd459f71461a
Jacob <79912360+JacobW22@users.noreply.github.com>
2023-08-23 19:59:40 +0200

    Swagger docs and updated random slides logic

--- app/youtube_helper/urls.py
+++ app/youtube_helper/urls.py
@@ -1,23 +1,23 @@
-"""youtube_helper URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from ythelperapp import views 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Youtube Helper Api",
+        default_version='v1',
+        description="Api for Youtube Helper application",
+        contact=openapi.Contact(email="jwis02202@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
 
@@ -54,6 +54,7 @@ urlpatterns = [
 
     # Api
 
+    path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/download_history/', views.RetrieveDownloadHistory.as_view({'get': 'list'})),
     path('api/prompts_history/', views.RetrievePromptsHistory.as_view({'get': 'list'})),
     path('api/filtered_comments_history/', views.RetrieveFilteredCommentsHistory.as_view({'get': 'list'})),
