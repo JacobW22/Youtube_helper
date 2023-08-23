18ee791ace9c1752f8f525abb7e17279bf473ff7
Jacob <79912360+JacobW22@users.noreply.github.com>
2023-08-23 15:35:23 +0200

    Fixed timezone problem and model bugs

--- app/ythelperapp/models.py
+++ app/ythelperapp/models.py
@@ -21,62 +21,66 @@ class user_data_storage(models.Model):
 
 
 class download_history_item(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, unique=False)
     title = models.CharField(max_length=255)
     link = models.CharField(max_length=255)
     saved_on =  models.DateTimeField(default=timezone.now)
     thumbnail_url = models.CharField(max_length=255)
 
     class Meta:
+        ordering = ['-id']
         verbose_name = "Download History Item"
         verbose_name_plural = "Download History Items"
 
     def __str__(self):
-        return str(f'{self.user} {id}th item')
+        return str(f'{self.user} {self.id}th item')
 
 
 class prompts_history_item(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, unique=False)
     title = models.CharField(max_length=255)
     link = models.CharField(max_length=255)
     saved_on =  models.DateTimeField(default=timezone.now)
 
     class Meta:
+        ordering = ['-id']
         verbose_name = "Prompts History Item"
         verbose_name_plural = "Prompts History Items"
 
     def __str__(self):
-        return str(f'{self.user} {id}th item')
+        return str(f'{self.user} {self.id}th item')
 
 
 
 class filtered_comments_history_item(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, unique=False)
     title = models.CharField(max_length=255)
     link = models.CharField(max_length=255)
     saved_on = models.DateTimeField(default=timezone.now)
 
     class Meta:
+        ordering = ['-id']
         verbose_name = "Filtered Comments History Item"
         verbose_name_plural = "Filtered Comments History Items"
 
     def __str__(self):
-        return str(f'{self.user} {id}th item')
+        return str(f'{self.user} {self.id}th item')
     
 
 
 class transferred_playlists_history_item(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, unique=False)
     title = models.CharField(max_length=255)
     link = models.CharField(max_length=255)
     saved_on = models.DateTimeField(default=timezone.now)
 
     class Meta:
+        ordering = ['-id']
         verbose_name = "Transferred Playlists History Item"
         verbose_name_plural = "Transferred Playlists History Items"
 
     def __str__(self):
-        return str(f'{self.user} {id}th item')
+        return str(f'{self.user} {self.id}th item')
 
 
 
