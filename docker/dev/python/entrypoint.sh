ee2f9dd38cd00ae6a3dee84aaa1c1583596e9b67
Jacob <79912360+JacobW22@users.noreply.github.com>
2023-08-31 17:30:07 +0200

    Github readme and fixed dockerfile

--- docker/dev/python/entrypoint.sh
+++ docker/dev/python/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-echo "Running command: '$*'"
-exec $*
+# Run Gunicorn
+echo "Running command: 'gunicorn --workers=5 --threads=2 youtube_helper.wsgi:application -b 0.0.0.0:8000'"
+
+python manage.py makemigrations && python manage.py migrate && gunicorn --workers=4 --threads=2 youtube_helper.wsgi:application -b 0.0.0.0:8000
